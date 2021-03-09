62f69edb740ee881c83c65aef670580e3b21b871
bcgood98 <bengood98@gmail.com>
2021-03-09 10:46:29 -0900

    implementation

--- implementation.c
+++ implementation.c
@@ -53,8 +53,6 @@
 #include <stddef.h>
 #include <sys/mman.h>
 
-#define SPACE 1600000
-
 /* Predefined helper functions */
 
 static void *__memset(void *s, int c, size_t n) {
@@ -104,7 +102,7 @@ static int __try_size_t_multiply(size_t *c, size_t a, size_t b) {
 
   /* If any of the arguments a and b is zero, everthing works just fine. */
   if ((a == ((size_t) 0)) ||
-      (b == ((size_t) 0))) {
+      (a == ((size_t) 0))) {
     *c = a * b;
     return 1;
   }
@@ -241,57 +239,12 @@ void removeNode(node* node){
 void __free_impl(void *);
 
 void *__malloc_impl(size_t size) {
-  /* STUB */  
-  int m = 1;
-  // size_t temp = size;
-  
-  // Determine the number of nodes demanded for the given size.
-  while (size >= SPACE*m) {
-	  m++;
-  }
-  
-  // Loop to remove nodes from the List of free space.
-  int i;
-  for (i=0; i < m; i++) {
-	  // Break if head is NULL; More should probably be done to handle this.
-	  if (head == NULL) {
-		  break;
-	  }
-	  removeNode(head);
-  }
-  
-  
+  /* STUB */
   return NULL;
 }
 
 void *__calloc_impl(size_t nmemb, size_t size) {
   /* STUB */
-  // compute total space needed by multiplying the number of elements by the size of an element.
-  size_t totalSpace = nmeb * size;
-  // int m = 1;
-  
-  // Check for overflow and return error if it occurs.
-  if (totalSpace < 0) {
-	  return NULL;
-  }
-  
-  // We should just need to call malloc.
-  
-  _malloc_impl(totalSpace);
-  
-/*   while (totalSpace >= SPACE*m) {
-	  m++;
-  }
-  
-  int i;
-  for (i = 0; i < m; i++) {
-	  if (head == NULL) {
-		  break;
-	  }
-	  
-	  removeNode(head);
-  } */
-  
   return NULL;  
 }
 
@@ -302,9 +255,6 @@ void *__realloc_impl(void *ptr, size_t size) {
 
 void __free_impl(void *ptr) {
   /* STUB */
-  void *p = mmap(NULL, SPACE, PROT_READ | PROT_WRITE, -1, 0);
-  
-  insertNode(p);
 }
 
 /* End of the actual malloc/calloc/realloc/free functions */
